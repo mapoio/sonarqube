bd1fc3ac144ab8f9b20f4e87a9eb6c820c670b6b
Jiean Zeng <16497636+mapoio@users.noreply.github.com>
2020-08-10 21:26:11 +0800

    Create sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,7 @@
+  
+#!/usr/bin/env bash
+
+chmod -R 644 lib/common/sonarqube-community-branch-plugin-*.jar 2>/dev/null
+chown -R sonarqube:sonarqube lib/common/sonarqube-community-branch-plugin-*.jar 2>/dev/null
+
+exec java -jar lib/sonar-application-"${SONAR_VERSION}".jar -Dsonar.log.console=true "$@"
